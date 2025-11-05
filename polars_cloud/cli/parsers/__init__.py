6022e65ac156b8895d006e56d6869df8e228895a
Ritchie Vink <ritchie46@gmail.com>
2025-11-05 15:29:32 +0100

    remove old

--- polars_cloud/cli/parsers/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Module containing the `argparse` CLI parser definition."""
-
-from polars_cloud.cli.parsers.main import create_parser
-
-__all__ = ["create_parser"]
--- polars_cloud/organization/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Code for managing organizations."""
-
-from polars_cloud.organization.organization import Organization
-
-__all__ = ["Organization"]
